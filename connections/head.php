17a6f7e5152684f5939b04a04813affb1b0ee9b9
Joey Lorenz Corpuz <paleraciojoey68@gmail.com>
2026-06-23 10:58:52 +0800

    feat: create head.php to standardize document metadata and asset inclusions

--- connections/head.php
+++ connections/head.php
@@ -1,17 +1,15 @@
 <?php
 
+$basePath = '/shoepee';
+
 $headContent = '<meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link rel="stylesheet"
                     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-                <link rel="icon" href="/../assets/images/shoepee_logo.png">
-                <link rel="stylesheet" href="/../assets/CSS/style.css">
-                <link rel="stylesheet" href="/../assets/CSS/playground.css">
-                <!-- for cart and favorites links -->
-                <link rel="icon" href="/shoepee/assets/images/shoepee_logo.png">
-                <link rel="stylesheet" href="/shoepee/assets/CSS/style.css">
-                <link rel="stylesheet" href="/shoepee/assets/CSS/playground.css">
+                <link rel="icon" href="' . $basePath . '/assets/images/shoepee_logo.png">
+                <link rel="stylesheet" href="' . $basePath . '/assets/CSS/style.css">
+                <link rel="stylesheet" href="' . $basePath . '/assets/CSS/playground.css">
                 ';
 
 ?>
